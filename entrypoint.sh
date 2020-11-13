d645f430e767b73d269d6355224efbb60d9e4b68
MK <marvinkome@gmail.com>
2020-11-13 22:54:24 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+cd "$1"
+
 if [ -z "$APPCENTER_ACCESS_TOKEN" ]; then
     echo "APPCENTER_ACCESS_TOKEN is required to run commands with the appcenter cli"
     exit 126
 fi
 
-
+echo "PWD $(pwd)"
 sh -c "appcenter codepush $*"
